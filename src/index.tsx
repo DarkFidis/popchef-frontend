7787467c57f1c67dbecd8c6be5a7818374c9d866
mseynaeve <matthieu.seynaeve@laposte.fr>
2022-05-13 15:55:24 +0200

    Add app index

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const MainApp = () => {
+  return (
+    <>
+      <div> Hello World </div>
+    </>
+  )
+}
+
+ReactDOM.render(<MainApp />, document.getElementById('root'))
