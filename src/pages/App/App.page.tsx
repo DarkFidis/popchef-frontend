65d1a8787e544f54955c9afbd937cabe7c99e74a
mseynaeve <matthieu.seynaeve@laposte.fr>
2022-05-17 16:11:04 +0200

    Refacto ApolloProvider

--- src/pages/App/App.page.tsx
+++ src/pages/App/App.page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Home} from "../Home";
+import {client} from "../../graphql/client";
+import {ApolloProvider} from "@apollo/client";
 
 export const App: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path={'/'} element={<Home />} />
-    </Routes>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+      </Routes>
+    </Router>
+  </ApolloProvider>
 )
--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import {MovieContainer} from "../../containers/MovieContainer/Movie.container";
-import {ApolloProvider} from "@apollo/client";
-import {client} from "../../graphql/client";
 
 export const Home: React.FC = () => {
   return (
-    <ApolloProvider client={client}>
-      <MovieContainer />
-    </ApolloProvider>
+    <MovieContainer />
   )
 }
 
